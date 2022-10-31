0ecd8032264c7a2336c52559afc9dc53c313cc63
HigoJhon <higojhon98@gmail.com>
2022-10-31 08:51:30 -0300

    Arrumando a questao 4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
@@ -130,6 +131,18 @@ class App extends React.Component {
             />
           ))
         }
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+        />
       </div>
     );
   }
